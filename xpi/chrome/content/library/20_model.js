a7a86ea726b4cd4a9c4e7962f2c7eda0e6cefe71
ku <ku0522a@gmail.com>
2010-01-01 13:55:55 +0900

    remove rejaw and update poster favicon urls.

--- xpi/chrome/content/library/20_model.js
+++ xpi/chrome/content/library/20_model.js
@@ -409,7 +409,7 @@ models.register(update({
 
 models.register({
 	name : 'WeHeartIt',
-	ICON : 'http://weheartit.com/img/favicon.ico',
+	ICON : 'http://weheartit.com/favicon.ico',
 	URL : 'http://weheartit.com/',
 	
 	check : function(ps){
@@ -457,7 +457,7 @@ models.register({
 
 models.register({
 	name : '4u',
-	ICON : 'http://www.straightline.jp/html/common/static/favicon.ico',
+	ICON : 'http://static.straightline.jp/html/common/static/favicon.ico',
 	
 	URL : 'http://4u.straightline.jp/',
 	
@@ -736,54 +736,6 @@ models.register({
 		});
 	},
 });
-
-models.register(update({}, AbstractSessionService, {
-	name : 'Rejaw',
-	ICON : 'http://rejaw.com/favicon.ico',
-
-	check : function(ps){
-		return (/(regular|photo|quote|link|conversation|video)/).test(ps.type) && !ps.file;
-	},
-	
-	post : function(ps){
-		return Rejaw.shout(joinText([ps.item, ps.itemUrl, ps.body, ps.description], '\n', true));
-	},
-	
-	shout : function(text){
-		return Rejaw.getToken().addCallback(function(token){
-			return request('http://rejaw.com/v1/conversation/shout.json', {
-				redirectionLimit : 0,
-				sendContent : update(token, {
-					text : text,
-				}),
-			});
-		});
-	},
-	
-	getAuthCookie : function(){
-		return getCookieString('rejaw.com', 'signin_email') || getCookieString('rejaw.com', 'signin_openid_url');
-	},
-	
-	getToken : function(){
-		var status = this.updateSession();
-		switch (status){
-		case 'none':
-			throw new Error(getMessage('error.notLoggedin'));
-			
-		case 'same':
-			if(this.token)
-				return succeed(this.token);
-			
-		case 'changed':
-			var self = this;
-			return request('http://rejaw.com/').addCallback(function(res){
-				return self.token = {
-					session : res.responseText.extract(/"session":"(.+?)"/)
-				};
-			});
-		}
-	},
-}));
 
 models.register(update({
 	name : 'Plurk',
@@ -1598,76 +1550,6 @@ models.register({
 		});
 	},
 });
-
-/*
-models.register({
-	name : 'Magnolia',
-	ICON : 'http://ma.gnolia.com/favicon.ico',
-	
-	getCurrentUser : function(){
-		return request('https://ma.gnolia.com/').addCallback(function(res){
-			var doc = convertToHTMLDocument(res.responseText);
-			var user = $x('//meta[@name="session-userid"]/@content', doc)
-			if(user=='')
-				throw new Error(getMessage('error.notLoggedin'));
-			return user;
-		});
-	},
-	
-	getApiKey : function(){
-		var self = this;
-		return request('http://ma.gnolia.com/account/applications').addCallback(function(res){
-			try {
-				return self.apikey = $x(
-					'id("api_key")/text()', 
-					convertToHTMLDocument(res.responseText)).replace(/[\n\r]+/g, '');
-			} catch(e) {
-				throw new Error(getMessage('error.notLoggedin'));
-			}
-		});
-	},
-	
-	getSuggestions : function(url){
-		// 同期でエラーが起きないようにする
-		return succeed().addCallback(function(){
-			return Magnolia.getCurrentUser().addCallback(function(user){
-				return request('https://ma.gnolia.com/people/' + user + '/tags');
-			}).addCallback(function(res){
-				var doc = convertToHTMLDocument(res.responseText);
-				return {
-					duplicated : false,
-					tags : $x('id("tag_cloud_1")/div/a/text()', doc, true).map(function(tag){
-						return {
-							name      : tag,
-							frequency : -1,
-						};
-					}),
-				}
-			});
-		});
-	},
-	
-	check : function(ps){
-		return (/(photo|quote|link|conversation|video)/).test(ps.type) && !ps.file;
-	},
-	
-	post : function(ps){
-		return Magnolia.getApiKey().addCallback(function(apikey){
-			return request('http://ma.gnolia.com/api/rest/1/bookmarks_add', {
-				queryString : {
-					api_key     : apikey,
-					url         : ps.itemUrl,
-					title       : ps.item,
-					description : ps.description,
-					private     : ps.private ? 1 : 0,
-					tags        : ps.tags ? ps.tags.join(' ') : '',
-					rating      : 0,
-				},
-			});
-		});
-	},
-});
-*/
 
 models.register({
 	name : 'Snipshot',
